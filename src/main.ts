f7bddf9fed3ce41ce8bae98052e074836e311036
Yurii Vydai <yuriivydai@gmail.com>
2026-03-30 20:32:43 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: 'https://next-admin.gaderia.com.ua',
+    origin: ['https://next-admin.gaderia.com.ua', 'http://localhost:3000'],
   });
   app.useStaticAssets(join(__dirname, '..', 'static'), { prefix: '/static' });
   await app.listen(process.env.PORT ?? 3000);
